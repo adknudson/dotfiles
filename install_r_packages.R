4d7da7d109bcb4c35ccf45ce2242e74384f25772
Alex Knudson <aknudson@nevada.unr.edu>
2020-05-04 11:33:27 -0700

    updated r packages

--- install_r_packages.R
+++ install_r_packages.R
@@ -1,23 +1,29 @@
-# R script to install my common R packages
 install.packages(c("devtools", "tidyverse"))
 
-install.packages(c("rmarkdown","gridExtra","kableExtra","rticles", "bookdown"))
-install.packages("pagedown")
+install.packages(c("rmarkdown",
+                   "gridExtra",
+                   "kableExtra",
+                   "rticles",
+                   "bookdown",
+                   "pagedown"))
 
 install.packages(c("coda","mvtnorm","loo","dagitty"))
 devtools::install_github("rmcelreath/rethinking")
 
+install.packages(c("here", "pacman"))
 
 install.packages("reticulate")
 reticulate::use_condaenv("greta-gpu")
 install.packages("greta")
-install.packages("bayesplot")
-install.packages("igraph")
-install.packages("DiagrammeR")
+install.packages(c("bayesplot", "igraph", "DiagrammeR"))
 
 if (!requireNamespace("BiocManager", quietly = TRUE)) {
   install.packages("BiocManager")
 }
-
 BiocManager::install("CNTools")
-install.packages("TCGA2STAT")"
+tcga2stat <-
+  "https://cran.r-project.org/src/contrib/Archive/TCGA2STAT/TCGA2STAT_1.2.tar.gz"
+download.file(tcga2stat,
+              destfile = "/tmp/Rtmpyn1r3y/downloaded_packages/TCGA2STAT_1.2.tar.gz")
+install.packages("/tmp/Rtmpyn1r3y/downloaded_packages/TCGA2STAT_1.2.tar.gz",
+                 repos = NULL, type = "source")
--- install_r_packages.sh
+++ install_r_packages.sh
@@ -9,26 +9,3 @@ sudo apt-get install -y libv8-dev libcurl4-openssl-dev libxml2-dev libssl-dev \
 libssh2-1-dev libgit2-dev
 
 
-echo "# R script to install my common R packages
-install.packages(c(\"devtools\", \"tidyverse\"))
-
-install.packages(c(\"rmarkdown\",\"gridExtra\",\"kableExtra\",\"rticles\"))
-
-install.packages(c(\"coda\",\"mvtnorm\",\"loo\",\"dagitty\"))
-devtools::install_github(\"rmcelreath/rethinking\")
-
-install.packages(\"greta\")
-install.packages(\"bayesplot\")
-install.packages(\"igraph\")
-install.packages(\"DiagrammeR\")
-
-if (!requireNamespace(\"BiocManager\", quietly = TRUE)) {
-  install.packages(\"BiocManager\")
-}
-
-BiocManager::install(\"CNTools\")
-install.packages(\"TCGA2STAT\")" > install_r_packages.R
-
-Rscript install_r_packages.R >> install_r_packages.out
-
-rm install_r_packages.R
