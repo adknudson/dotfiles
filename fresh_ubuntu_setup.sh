e69f91853ceddec4d1b73540ad3e9e0a680064a2
Alex Knudson <aknudson@nevada.unr.edu>
2020-05-15 15:28:02 -0700

    script for fresh ubuntu installs

---
+++ fresh_ubuntu_setup.sh
@@ -0,0 +1,40 @@
+# General for new install
+sudo apt-get install -y \
+	git \
+	texlive-full \
+	tree \
+	htop \
+	chrome-gnome-shell \
+	gnome-tweaks \
+	okular
+
+# Sublime Text
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt-get install apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install sublime-text
+
+# Install R v4.0 on Ubuntu 18.04
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran40/'
+sudo apt-get update
+sudo apt install r-base-dev
+
+# Packages necessary for 'devtools', 'tidyverse'
+sudo apt-get install -y \
+	libv8-dev \
+	libcurl4-openssl-dev \
+	libxml2-dev libssl-dev \
+	libssh2-1-dev
+
+sudo apt-get install -y libcurl4-gnutls-dev
+sudo apt-get install -y libgit2-dev
+
+wget -P ~/Downloads https://julialang-s3.julialang.org/bin/linux/x64/1.4/julia-1.4.1-linux-x86_64.tar.gz
+tar -xzvf ~/Downloads/julia-1.4.1-linux-x86_64.tar.gz
+echo "# >>> JULIA RELATED STUFF
+export PATH=\"\$PATH:\$HOME/julia-1.4.1/bin\"
+export JULIA_NUM_THREADS=\`nproc\`
+# <<<" >> .bashrc
+source .bashrc
