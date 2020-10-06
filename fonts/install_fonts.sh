4226137bb9ed95661232f7573c71eee63a35c599
Alex Knudson <aknudson@nevada.unr.edu>
2020-10-05 17:19:41 -0700

    added fira code font

--- fonts/install_fonts.sh
+++ fonts/install_fonts.sh
@@ -1,6 +1,13 @@
-mkdir -p tmp
-unzip *.zip -d tmp
 mkdir -p ~/.fonts
+
+mkdir -p tmp
+unzip Source_Code_Pro.zip -d tmp
 cp tmp/*.ttf ~/.fonts
-fc-cache -f -v
 rm -rf tmp
+
+mkdir -p tmp2
+unzip Fira_Code_v*.zip -d tmp2
+cp tmp2/ttf/*.ttf ~/.fonts
+rm -rf tmp2
+
+fc-cache -f -v
